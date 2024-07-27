44d415d51bdc523f0c9c5db7b6ef9c94a390a55c
Corey Bickford <cb6@live.ca>
2024-07-26 22:57:23 -0700

    Create get request for the details of a single item, update readme

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 -   Create basic controller and route files
 
 ## Develop Branch
+
+### Feature/ GET single item details
+
+-   define inventory page routes
+-   link page routes to inventory controller
+-   define GET request for details of a single item
+-   awaiting database for testing
--- controllers/inventory-controllers.js
+++ controllers/inventory-controllers.js
@@ -1,3 +1,27 @@
 const knex = require("knex")(require("../knexfile"));
 
-module.exports = {};
+//get detail for a specific item:
+const details = async (req, res) => {
+	try {
+		const itemFound = await knex("inventory").where({
+			id: req.params.id,
+		});
+
+		if (itemFound.length === 0) {
+			return res
+				.status(404)
+				.json({
+					message: `Item ID ${req.params.id} not found`,
+				});
+		}
+
+		const itemDetails = itemFound[0];
+		res.json(itemDetails);
+	} catch {
+		res.status(500).json({
+			message: `Unable to retrieve data for item ID ${req.params.id}`,
+		});
+	}
+};
+
+module.exports = { details };
