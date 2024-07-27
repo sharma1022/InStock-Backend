59e6e244714101685b1c0da3e8e529eb9e6bcab2
Corey Bickford <cb6@live.ca>
2024-07-26 22:54:18 -0700

    Declare route for inventory page

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+//connect to controller:
+const inventoryController = require("../controllers/inventory-controllers");
+
+//delegate request processing to the controller functions:
+//all items:
+router.route("/");
+//single item
+router.route("/:id").get(inventoryController.details);
+
 module.exports = router;
