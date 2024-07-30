bb49e44aafcba5f135f210cc48e3cf9c1adc732f
Daniel <dbeaulne1@gmail.com>
2024-07-30 06:25:21 -0400

    code and test delete warehouse api code and route

--- routes/warehouses-routes.js
+++ routes/warehouses-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const warehouseController = require("../controllers/warehouse-controllers");
 
 router.route("/:id").get(warehouseController.findOne);
+router.route("/:id").delete(warehouseController.deleteWarehouse);
 
 module.exports = router;
