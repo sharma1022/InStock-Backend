7df442513d76fba0632e20e5fc41bdaa065c13e8
Corey Bickford <cb6@live.ca>
2024-08-01 21:41:21 -0700

    Clarify route names
    modified:   routes/inventory-routes.js
    modified:   routes/inventory-routes.js

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 //connect to controller:
 const inventoryController = require("../controllers/inventory-controllers");
 
-//delegate request processing to the controller functions:
+//delegate request processing to the controller functions
 
 //add a new item:
 router.route("/").post(inventoryController.addItem);
 
-//single item
+//get the details of a single item:
 router.route("/:id").get(inventoryController.details);
 
 module.exports = router;
